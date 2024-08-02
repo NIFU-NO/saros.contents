d20510b1e35de332937ab08f5f94d9ea1928dae6
sda030 <stephus.daus@gmail.com>
2024-08-02 21:11:57 +0200

    Re-inserted saros.base import for now

--- R/data.R
+++ R/data.R
@@ -1,3 +1,7 @@
 #' @inherit saros.base::ex_survey description source references title
 #' @export
 ex_survey <- saros.base::ex_survey
+
+.not_used_fns <- function() {
+  saros.base::read_default_draft_report_args
+}
